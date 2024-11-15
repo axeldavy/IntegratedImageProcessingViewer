3df20ae4fbfae9cf29c5658ae036d7aca9a07a77
Axel Davy <axel.davy@ens-cachan.fr>
2024-11-15 23:59:54 +0100

    example: Pass correct value to width and height

--- examples/blur.py
+++ examples/blur.py
@@ -40,9 +40,9 @@ def read_image_and_add_process(path):
 
 path = os.path.join(os.path.dirname(__file__), "lenapashm.png")
 with dcg.Window(C, primary=True):
-    with dcg.ChildWindow(C, width=-200, height=-1, no_newline=True, no_scrollbar=True):
+    with dcg.ChildWindow(C, width=-200, height=0, no_newline=True, no_scrollbar=True):
         viewer = ViewerElement(C, [path], 1, read_image_and_add_process)
-    with dcg.ChildWindow(C, width=-1, height=-1):
+    with dcg.ChildWindow(C, width=0, height=0):
         dcg.Slider(C, label="Noise std.", shareable_value=noise_std, min_value=0, max_value=50, width=100, callback=viewer.refresh_image)
         dcg.Slider(C, label="Blur width", shareable_value=blur_width, min_value=1, format='int', max_value=10, width=100, callback=viewer.refresh_image)
         dcg.Slider(C, label="Blur height", shareable_value=blur_height, min_value=1, format='int', max_value=10, width=100, callback=viewer.refresh_image)
